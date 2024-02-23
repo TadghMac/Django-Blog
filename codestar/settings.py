35cf42bdb013836860e70ad0060988ffac1ee3ee
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-02-23 10:19:54 +0000

    csrf trusted origins

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True
 
 ALLOWED_HOSTS = ["8000-tadghmac-djangoblog-h8prfdp7qla.ws-eu108tpod.io", 
 '.herokuapp.com']
@@ -90,6 +90,11 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+CSRF_TRUSTED_ORIGINS = [
+    "https://*.gitpod.com",
+    "https://*.herokuapp.com"
+    "8000-tadghmac-djangoblog-h8prfdp7qla.ws-eu108tpod.io"
+]
 
 
 # Password validation
